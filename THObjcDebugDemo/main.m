558f9b2c4763004e5c1b0fe916684a88ed66208b
cocoon <yangle@thejoyrun.com>
2022-02-10 16:41:00 +0800

    objc4 source code compilation

---
+++ THObjcDebugDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  THObjcDebugDemo
+//
+//  Created by Terence Yang on 2022/2/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray *arr = [[NSArray alloc] init];
+        
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
